ddcf6162ab158a2d90e95c4142e761600690423a
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2023-02-05 01:36:30 -0300

    Add heading font variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,13 @@
 module.exports = {
 	content: ['./*.html', './src/**/*.css', './src/*/**.tsx'],
+	theme: {
+		extend: {
+			fontFamily: {
+				heading: ['Inter, sans-serif', { fontFeatureSettings: '"salt"' }],
+			},
+			boxShadow: {
+				ring: 'inset 0 0 0 1px rgba(255, 255, 255, 0.1)',
+			},
+		},
+	},
 }
