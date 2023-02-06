0d0b6e4c5650c8e2bd9921684266c18032579318
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2023-02-05 21:33:23 -0300

    fix: tailwind content sources

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-	content: ['./*.html', './src/**/*.css', './src/*/**.tsx'],
-	theme: {
-		extend: {
-			fontFamily: {
-				heading: ['Inter, sans-serif', { fontFeatureSettings: '"salt"' }],
-			},
-			boxShadow: {
-				ring: 'inset 0 0 0 1px rgba(255, 255, 255, 0.1)',
-			},
-		},
-	},
-}
+  content: ["./*.html", "./src/**/*.css", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ["Inter, sans-serif", { fontFeatureSettings: '"salt"' }],
+      },
+      boxShadow: {
+        ring: "inset 0 0 0 1px rgba(255, 255, 255, 0.1)",
+      },
+    },
+  },
+};
