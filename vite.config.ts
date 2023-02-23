d6f250799969fbeef78d9d0af7b27d3cea72aad6
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2023-02-23 17:02:40 -0300

    Apply project source to timers

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { resolve } from 'path'
 
 import react from '@vitejs/plugin-react'
 import webfontDownload from 'vite-plugin-webfont-dl'
+import replace from '@rollup/plugin-replace'
+
+const __PROJECT_SOURCE__ =
+	'https://github.com/ZEBAS204/professional-looking-timers'
 
 export default defineConfig((configEnv) => {
 	const isDevelopment = configEnv.mode === 'development'
@@ -14,11 +18,22 @@ export default defineConfig((configEnv) => {
 				// TODO: use true type font
 				'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;800&display=swap',
 			]),
+			replace({
+				include: ['src/components/timers/**/index.tsx'],
+				values: {
+					__TIMER_URL__: (filePath: string) => {
+						// We know that src is the root of our project, so we can remove everything on its left and,
+						// get the timer folder, removing everything to the right of the last slash in the path.
+						return (
+							__PROJECT_SOURCE__ +
+							filePath.substring(filePath.indexOf('/src')).split('index')[0]
+						)
+					},
+				},
+			}),
 		],
 		define: {
-			__PROJECT_SOURCE__: JSON.stringify(
-				'https://github.com/ZEBAS204/professional-looking-timers'
-			),
+			__PROJECT_SOURCE__: JSON.stringify(__PROJECT_SOURCE__),
 		},
 		resolve: {
 			alias: {
