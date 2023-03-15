051260e3f3c39d9c7587ba64c1baf648a3ae9973
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2023-03-15 19:55:47 -0300

    Add Vercel Analytics
    - Added `@vercel/analytics` to dependencies.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { inject } from '@vercel/analytics'
 import "./index.css";
 import App from "app/app";
+
+// Vercel Analytics
+inject()
 
 const root = createRoot(document.getElementById("root"));
 
