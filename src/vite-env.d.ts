c8ded0543cd9311ea424937c457bcc2a0abdb0f1
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2023-03-16 20:09:38 -0300

    Add timer and attribution types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,28 @@
 /// <reference types="vite/client" />
 
+interface ComponentAttribution {
+	source: string
+	isIspiredBy?: boolean
+	sourceName?: string
+	attribution?: Array<{
+		name: string
+		url?: string
+	}>
+}
+
 declare global {
 	const __PROJECT_SOURCE__: string
+
+	interface TimerComponent {
+		component: (...args: unknown[]) => JSX.Element
+		title: string
+		description?: string
+		content?: React.ReactNode
+		source: '__TIMER_URL__'
+		attribution?: ComponentAttribution
+	}
+
+	type Attributions = ComponentAttribution
 }
 
 export {}
